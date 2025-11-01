d316d8136afcfc5d1238a1029de47d9c694fdcea
Sonali Rout <sonalirout364@gmail.com>
2025-11-01 23:29:49 +0530

    address fix again1

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -59,7 +59,7 @@ const Cart = () => {
   useEffect(() => {
     const fetchAddresses = async () => {
       try {
-        const { data } = await axios.post("/api/address/get", {}, { withCredentials: true });
+        const { data } = await axios.post("/api/address/get", FormData, { withCredentials: true });
         if (data.success && data.addresses.length > 0) {
           setAddresses(data.addresses);
           setSelectedAddress(data.addresses[0]);
